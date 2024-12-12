d2989ca87124c79cf1cfa8a0e2e8776c3f61ffce
Sascha Stieglitz <sascha.stieglitz@web.de>
2024-12-12 12:48:28 +0100

    show saleschannels in list

--- src/Resources/app/administration/src/module/event-config/page/mapp-event-config-list/index.js
+++ src/Resources/app/administration/src/module/event-config/page/mapp-event-config-list/index.js
@@ -86,7 +86,6 @@ Component.register('mapp-event-config-list', {
                 property: 'salesChannels',
                 dataIndex: 'salesChannels',
                 label: 'Sales Channel',
-                sortable: false,
                 allowResize: true,
                 multiLine: true
             }, {
@@ -107,6 +106,9 @@ Component.register('mapp-event-config-list', {
                 .search(this.mappEventCriteria, Shopware.Context.api)
                 .then((response) => {
                     this.items = response;
+                    this.items.forEach((item, index, items) => {
+                        items[index].salesChannels = item.salesChannels.map(s => s.name).join(", ");
+                    });
                     this.total = response.total;
                     this.isLoading = false;
                 });
